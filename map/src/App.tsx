4ea1ab305f3715b53cae0f8619760af72b66a68d
freercurse <freerjoseph@yahoo.co.yk>
2022-08-03 20:07:05 +0100

    fleshing out main screen

--- map/src/App.tsx
+++ map/src/App.tsx
@@ -1,10 +1,16 @@
+import { QuestionCircleOutlined } from '@ant-design/icons';
 import React from 'react';
 import './styles/main.scss'
 
 function App() {
   return (
     <div className='Title'>
-      <h1>Map</h1>
+      <h1 className='entry'>Map</h1>
+      <div className='EntryForm'>
+        <input className='seed' type="text"/>
+        <QuestionCircleOutlined />
+      </div>
+      
     </div>
   );
 }
