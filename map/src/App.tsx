fbb72f61d7c90a2288a526d79de9e956d7da14df
freercurse <freerjoseph@yahoo.co.yk>
2022-09-06 21:47:02 +0100

    added entry and exit animations to main input

--- map/src/App.tsx
+++ map/src/App.tsx
@@ -6,25 +6,29 @@ import React, { createRef, useState } from 'react';
 function App() {
   const [game, setGame] = useState(false)
   const boardRef = createRef<HTMLInputElement>()
+  const containerRef  = createRef<HTMLCanvasElement>()
 
-  function entergame(event:any) {
+  function toggleGame(event:any) {
     const board = boardRef.current
+    const container = containerRef.current
     
-    if(event.code === "Enter"){
-      board?.classList.toggle('is-game-opening',true)  
+    if(event?.code === "Enter"){
+      board?.classList.toggle('is-game-opening',true)        
 
-      setTimeout(() => {setGame(true)},5000)
-          
+      setTimeout(() => {setGame(true)},5000)          
     }
 
-    
+    if(event === "endGame"){
+      container?.classList.toggle('is-game-closing')        
+      setTimeout(() => {setGame(false)},5000)
+    }
   }
 
   return (
     <div className='Title'>
-      <h1 className='entry'>Map</h1>
+      <h1 className='entry' onClick={() =>toggleGame("endGame")}>Map</h1>
       {!game ?<div className='EntryForm'>        
-        <input onKeyUp={(e) => entergame(e)} ref={boardRef} className='seed' type="number" maxLength={12}/>              
+        <input onKeyUp={(e) => toggleGame(e)} ref={boardRef} className='seed' type="number" maxLength={12}/>              
         <Tooltip title="type in a random number to use as a seed. 1-12" placement='rightTop'>
            <QuestionCircleOutlined />
         </Tooltip>       
@@ -33,6 +37,8 @@ function App() {
         <canvas
           width={400}
           height={500}
+          className={"game"}
+          ref={containerRef}
         />
       </div>}
       
